139ea0b8e26c6736cf74608880d366d56f33d101
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2022-02-21 18:25:33 -0400

    Modified AddEmployeeDialog
    added placeholder text and increased input fields length

--- src/SWE4403_UI_Dialogs/SWE4403_AddEmployeeDialog.java
+++ src/SWE4403_UI_Dialogs/SWE4403_AddEmployeeDialog.java
@@ -9,7 +9,7 @@ import SWE4403_UI_TextFields.SWE4403_DateTextField;
 import SWE4403_UI_TextFields.SWE4403_NormalTextField;
 import SWE4403_UI_TextFields.SWE4403_WageTextField;
 
-public class SWE4403_AddEmployeeDialog extends SWE4403_AddDialog {
+public class SWE4403_AddEmployeeDialog extends SWE4403_ConfirmationDialog {
 
 	private JTextField jobTitleTF, wageTF, startDateTF, endDateTF;
 	private JTextArea textArea;
@@ -21,13 +21,13 @@ public class SWE4403_AddEmployeeDialog extends SWE4403_AddDialog {
 		 * TEXTFIELD SETTINGS
 		 */
 		jobTitleTF = new SWE4403_NormalTextField("Job Title");
-		jobTitleTF.setBounds(10, 11, 344, 48);
+		jobTitleTF.setBounds(10, 11, 424, 48);
 		wageTF = new SWE4403_WageTextField("Wage");
-		wageTF.setBounds(10, 127, 344, 48);
+		wageTF.setBounds(10, 127, 424, 48);
 		startDateTF = new SWE4403_DateTextField("Start Date");
-		startDateTF.setBounds(10, 70, 160, 48);
+		startDateTF.setBounds(10, 70, 200, 48);
 		endDateTF = new SWE4403_DateTextField("End Date");
-		endDateTF.setBounds(194, 70, 160, 48);
+		endDateTF.setBounds(234, 70, 200, 48);
 
 		contentPanel.add(jobTitleTF);
 		contentPanel.add(wageTF);
@@ -40,7 +40,7 @@ public class SWE4403_AddEmployeeDialog extends SWE4403_AddDialog {
 				"JComboBox containing all the existing employees will be in here to specify the superior of this current employee");
 		textArea.setLineWrap(true);
 		textArea.setEditable(false);
-		textArea.setBounds(10, 186, 344, 159);
+		textArea.setBounds(10, 186, 424, 159);
 		contentPanel.add(textArea);
 	}
 
