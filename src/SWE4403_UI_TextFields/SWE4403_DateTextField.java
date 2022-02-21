86f8cdf61063064d863f566de0116ef44bf5b7b3
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2022-02-21 18:26:17 -0400

    Update SWE4403_DateTextField.java
    Added placeholder for date textfield

--- src/SWE4403_UI_TextFields/SWE4403_DateTextField.java
+++ src/SWE4403_UI_TextFields/SWE4403_DateTextField.java
@@ -1,9 +1,41 @@
 package SWE4403_UI_TextFields;
 
-public class SWE4403_DateTextField extends SWE4403_SensitiveTextField {
+import java.awt.Color;
+import java.awt.event.FocusEvent;
+import java.awt.event.FocusListener;
+
+import SWE4403_UI_Utilities.SWE4403_PlaceholderFont;
+import SWE4403_UI_Utilities.SWE4403_StandardFont;
+
+public class SWE4403_DateTextField extends SWE4403_SensitiveTextField implements FocusListener {
 
 	public SWE4403_DateTextField(String title) {
 		super(title);
+		addFocusListener(this);
+		noFocusText();
+	}
+
+	// This check is to add placeholder text
+	@Override
+	public void focusGained(FocusEvent e) {
+		if (getFont() == SWE4403_PlaceholderFont.getInstance()) {
+			setText("");
+			setForeground(Color.BLACK);
+			setFont(SWE4403_StandardFont.getInstance());
+		}
+	}
+
+	@Override
+	public void focusLost(FocusEvent e) {
+		if (getText().isBlank()) {
+			noFocusText();
+		}
+	}
+
+	private void noFocusText() {
+		setFont(SWE4403_PlaceholderFont.getInstance());
+		setForeground(Color.GRAY);
+		setText("dd/mm/yyyy");
 	}
 
 }
