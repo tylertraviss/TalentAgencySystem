99961be5c11a4f1c18fb1e6da1515e5d9838662e
Elliot-Chin <ychin@unb.ca>
2022-02-22 21:16:59 -0400

    Added Color Settings

--- src/SWE4403_UI_Buttons/SWE4403_EditEmployeeButton.java
+++ src/SWE4403_UI_Buttons/SWE4403_EditEmployeeButton.java
@@ -1,9 +1,12 @@
 package SWE4403_UI_Buttons;
 
+import java.awt.Color;
+
 public class SWE4403_EditEmployeeButton extends SWE4403_Button {
 
 	public SWE4403_EditEmployeeButton(String text) {
 		super(text);
+		this.setForeground(Color.WHITE);
 	}
 
 }
