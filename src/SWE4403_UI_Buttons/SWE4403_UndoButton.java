636971f36533751aa518f0ff796f00e1cdfdd961
Elliot-Chin <ychin@unb.ca>
2022-02-22 21:15:56 -0400

    Create SWE4403_UndoButton.java

---
+++ src/SWE4403_UI_Buttons/SWE4403_UndoButton.java
@@ -0,0 +1,12 @@
+package SWE4403_UI_Buttons;
+
+import java.awt.Color;
+
+public class SWE4403_UndoButton extends SWE4403_Button {
+
+	public SWE4403_UndoButton(String name) {
+		super(name);
+		this.setForeground(Color.WHITE);
+	}
+
+}
