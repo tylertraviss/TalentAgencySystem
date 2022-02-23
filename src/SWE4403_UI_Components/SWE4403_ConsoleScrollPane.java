23723645dbc8536145fd9e3a95833519a28b8ee6
Elliot-Chin <ychin@unb.ca>
2022-02-22 21:17:13 -0400

    Added Listener to auto scroll

--- src/SWE4403_UI_Components/SWE4403_ConsoleScrollPane.java
+++ src/SWE4403_UI_Components/SWE4403_ConsoleScrollPane.java
@@ -1,19 +1,37 @@
 package SWE4403_UI_Components;
 
-import java.awt.Component;
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.AdjustmentEvent;
+import java.awt.event.AdjustmentListener;
 
 import javax.swing.JScrollPane;
 
 import SWE4403_UI_Utilities.SWE4403_RoundedBorder;
 
-public class SWE4403_ConsoleScrollPane extends JScrollPane {
+public class SWE4403_ConsoleScrollPane extends JScrollPane implements AdjustmentListener {
 
-	public SWE4403_ConsoleScrollPane(Component c) {
-		super(c);
+	private static SWE4403_ConsoleScrollPane instance = null;
+
+	public SWE4403_ConsoleScrollPane() {
+		super(SWE4403_ConsolePanel.getInstance());
 		this.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_ALWAYS);
+		// hide the vertical scroll bar
 		this.getVerticalScrollBar().setPreferredSize(new Dimension(0, 0));
 		this.setBorder(SWE4403_RoundedBorder.getInstance());
+		this.setBackground(Color.BLACK);
+		getVerticalScrollBar().addAdjustmentListener(this);
+	}
+
+	public static SWE4403_ConsoleScrollPane getInstance() {
+		if (instance == null)
+			instance = new SWE4403_ConsoleScrollPane();
+		return instance;
+	}
+
+	@Override
+	public void adjustmentValueChanged(AdjustmentEvent e) {
+		e.getAdjustable().setValue(e.getAdjustable().getMaximum());
 	}
 
 }
