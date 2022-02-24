a6e41804c71d9470dda8402942675dd8f0d1ed65
Elliot-Chin <ychin@unb.ca>
2022-02-23 23:29:31 -0400

    Update SWE4403_ConsolePanel.java
    Moved creation of JLabels to the log method instead of creating them in the buttonHandler class

--- src/SWE4403_UI_Components/SWE4403_ConsolePanel.java
+++ src/SWE4403_UI_Components/SWE4403_ConsolePanel.java
@@ -1,19 +1,24 @@
 package SWE4403_UI_Components;
 
 import java.awt.Color;
-import java.awt.Component;
 import java.awt.Dimension;
 
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JPanel;
 
+import SWE4403_UI_JLabels.SWE4403_AcceptedLabel;
+import SWE4403_UI_JLabels.SWE4403_ErrorLabel;
+import SWE4403_UI_JLabels.SWE4403_NormalLabel;
+import SWE4403_UI_JLabels.SWE4403_WarningLabel;
+import SWE4403_UI_Utilities.SWE4403_MessageType;
+
 public class SWE4403_ConsolePanel extends JPanel {
 
 	private static SWE4403_ConsolePanel instance = null;
 
 	private SWE4403_ConsolePanel() {
-		// this.setLayout(new FlowLayout(FlowLayout.LEFT));
+//		this.setLayout(new FlowLayout(FlowLayout.LEFT));
 		this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
 		this.setBackground(Color.BLACK);
 	}
@@ -25,8 +30,25 @@ public class SWE4403_ConsolePanel extends JPanel {
 		return instance;
 	}
 
-	public void log(Component c) {
-		this.add(c);
+	public void log(String message, SWE4403_MessageType type) {
+
+		switch (type) {
+		case NORMAL:
+			this.add(new SWE4403_NormalLabel(message));
+			break;
+		case WARNING:
+			this.add(new SWE4403_WarningLabel(message));
+			break;
+		case ERROR:
+			this.add(new SWE4403_ErrorLabel(message));
+			break;
+		case ACCEPT:
+			this.add(new SWE4403_AcceptedLabel(message));
+			break;
+		default:
+			this.add(new SWE4403_ErrorLabel("Unknown message type"));
+		}
+
 		this.add(Box.createRigidArea(new Dimension(10, 10)));
 		this.revalidate();
 		this.repaint();
