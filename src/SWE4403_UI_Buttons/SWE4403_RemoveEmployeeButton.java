20bb06837c7c490ae23cb65eafd766f06004150a
Elliot-Chin <ychin@unb.ca>
2022-02-23 23:28:45 -0400

    Update SWE4403_RemoveEmployeeButton.java

--- src/SWE4403_UI_Buttons/SWE4403_RemoveEmployeeButton.java
+++ src/SWE4403_UI_Buttons/SWE4403_RemoveEmployeeButton.java
@@ -4,7 +4,6 @@ public class SWE4403_RemoveEmployeeButton extends SWE4403_Button {
 
 	public SWE4403_RemoveEmployeeButton(String text) {
 		super(text);
-		// TODO Auto-generated constructor stub
 	}
 
 }
