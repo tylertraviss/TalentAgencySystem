cf622084fa017a361be6b9a54e16a68a0cb9821d
Elliot-Chin <ychin@unb.ca>
2022-02-23 23:28:40 -0400

    removed color

--- src/SWE4403_UI_Buttons/SWE4403_UndoButton.java
+++ src/SWE4403_UI_Buttons/SWE4403_UndoButton.java
@@ -1,12 +1,9 @@
 package SWE4403_UI_Buttons;
 
-import java.awt.Color;
-
 public class SWE4403_UndoButton extends SWE4403_Button {
 
 	public SWE4403_UndoButton(String name) {
 		super(name);
-		this.setForeground(Color.WHITE);
 	}
 
 }
