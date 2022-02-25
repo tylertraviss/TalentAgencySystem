58145f23e482dbc80b0858d4da61736298735dcc
Tyler C. Travis <79883329+tylertraviss@users.noreply.github.com>
2022-02-25 12:27:27 -0400

    Updating Person and Company with specifications declared in UML

--- src/src/com/company/Company.java
+++ src/src/com/company/Company.java
@@ -1,4 +1,60 @@
 package com.company;
+import java.util.List;
 
 public class Company {
+
+    private String name;
+    private String hqLoc;
+    private List<Employee> employees;
+    private List<Client> clients;
+    private Company instance; // Memento Later
+    private double totalIncome;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getHqLoc() {
+        return hqLoc;
+    }
+
+    public void setHqLoc(String hqLoc) {
+        this.hqLoc = hqLoc;
+    }
+
+    public List<Employee> getEmployees() {
+        return employees;
+    }
+
+    public void setEmployees(List<Employee> employees) {
+        this.employees = employees;
+    }
+
+    public List<Client> getClients() {
+        return clients;
+    }
+
+    public void setClients(List<Client> clients) {
+        this.clients = clients;
+    }
+
+    public Company getInstance() {
+        return instance;
+    }
+
+    public void setInstance(Company instance) {
+        this.instance = instance;
+    }
+
+    public double getTotalIncome() {
+        return totalIncome;
+    }
+
+    public void setTotalIncome(double totalIncome) {
+        this.totalIncome = totalIncome;
+    }
 }
--- src/src/com/company/Person.java
+++ src/src/com/company/Person.java
@@ -1,4 +1,41 @@
 package com.company;
 
 public class Person {
+
+    private String name;
+    private int age;
+    private String gender;
+    private String nationality;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
 }
