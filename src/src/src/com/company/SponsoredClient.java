a9835b674905f660973a643febc306d42a3cc071
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2022-03-15 22:57:30 -0300

    added toString method

--- src/src/src/com/company/SponsoredClient.java
+++ src/src/src/com/company/SponsoredClient.java
@@ -1,44 +1,23 @@
 package src.src.com.company;
 
-public class SponsoredClient{
+public class SponsoredClient {
     private SponsorshipMediator sponsorshipMediator;
-    private Musician musician;
-    private Actor actor;
-    private Athlete athlete;
+    private Client client;
     private String name;
 
-
-
-    public SponsoredClient(Musician musician, SponsorshipMediator sponsorshipMediator){
-        this.sponsorshipMediator= sponsorshipMediator;
-        this.musician= musician;
-        Person person= (Person) musician;
-        this.name= person.getName();
+    public SponsoredClient(Client client, SponsorshipMediator sponsorshipMediator) {
+        this.sponsorshipMediator = sponsorshipMediator;
+        this.client = client;
+        this.name = client.getName();
 
     }
 
-
-    public SponsoredClient(Actor actor, SponsorshipMediator sponsorshipMediator){
-        this.sponsorshipMediator= sponsorshipMediator;
-        Person person= (Person) actor;
-        this.name= person.getName();
+    public String getName() {
+        return name;
     }
 
-
-    public SponsoredClient(Athlete athlete, SponsorshipMediator sponsorshipMediator){
-        this.sponsorshipMediator= sponsorshipMediator;
-        Person person= (Person) athlete;
-        this.name= person.getName();
-
+    public String toString() {
+        return name;
     }
-
-    public String getName(){
-            return name;
-    }
-
-
-
-
-
 
 }
