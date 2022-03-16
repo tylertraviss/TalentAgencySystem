16c127c793b5e551dcce2f3ad45daba4d8aa626b
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2022-03-15 22:59:20 -0300

    Print to console and to text File is working

--- src/UI/dialogs/PrintDialog.java
+++ src/UI/dialogs/PrintDialog.java
@@ -1,14 +1,22 @@
 package UI.dialogs;
 
+import java.awt.Desktop;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
 
 import javax.swing.JFrame;
 
 import UI.textarea.ConsoleTA;
 import src.src.com.company.Company;
+import src.src.com.company.SponsorshipMediator;
+import src.src.com.company.TextFileAudit;
 
 public class PrintDialog extends AbstractEditDialog {
+
+	private static final String SEPERATOR = System.lineSeparator() + "---------------------------------------"
+			+ System.lineSeparator();
 
 	public PrintDialog(JFrame parentFrame) {
 		super("Print Company Stats", null, parentFrame);
@@ -20,20 +28,66 @@ public class PrintDialog extends AbstractEditDialog {
 		addButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+				try {
+					new TextFileAudit();
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+
+				File f = new File("GeneratedAudit.txt");
+				Desktop desktop = Desktop.getDesktop();
+				if (!f.exists()) {
+					// open the error dialog and show that the file does not exist
+					return;
+				}
+				try {
+					desktop.open(f);
+				} catch (IOException e1) {
+					e1.printStackTrace();
+				}
+				dispose();
 			}
 		});
 
 		removeButton.addActionListener(new ActionListener() {
 			@Override
 			public void actionPerformed(ActionEvent e) {
+
 				var company = Company.getInstance();
+				var console = ConsoleTA.getInstance();
+				var sponsorshipMediator = SponsorshipMediator.getInstance();
+
+				console.log(SEPERATOR);
+
+				console.log("\t\tCLIENT LIST");
 
 				if (company.getClients().size() == 0)
-					ConsoleTA.getInstance().log("No Clients Yet");
+					console.log("No Clients Yet");
 				else
 					for (var client : company.getClients())
-						ConsoleTA.getInstance().log(client.toConsole());
+						console.log(client.toConsole());
 
+				console.log(SEPERATOR);
+
+				console.log("\t\tEMPLOYEE LIST");
+
+				if (company.getEmployees().size() == 0)
+					console.log("No Employees Yet");
+				else
+					for (var emp : company.getEmployees())
+						console.log(emp.toConsole());
+
+				console.log(SEPERATOR);
+
+				console.log("\t\tSPONSOR LIST");
+
+				if (sponsorshipMediator.getSponsorships().size() == 0)
+					console.log("No Sponsors Yet");
+				else
+					for (var sp : sponsorshipMediator.getSponsorships())
+						console.log(sp.toConsole());
+
+				console.log(SEPERATOR);
 				dispose();
 			}
 
