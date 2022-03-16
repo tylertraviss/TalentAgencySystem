edaa8ac879f56447a5473b7b07441dd5abbf339a
Elliot-Chin <48703126+Elliot-Chin@users.noreply.github.com>
2022-03-15 22:57:08 -0300

    Added toString

--- src/src/src/com/company/Sponsor.java
+++ src/src/src/com/company/Sponsor.java
@@ -1,20 +1,21 @@
 package src.src.com.company;
 
-public class Sponsor{
+public class Sponsor {
     private SponsorshipMediator sponsorshipMediator;
     private String sponsorName;
 
-    public Sponsor(String sponsorName, SponsorshipMediator sponsorMediator){
-        this.sponsorshipMediator= sponsorMediator;
-        this.sponsorName= sponsorName;
+    public Sponsor(String sponsorName, SponsorshipMediator sponsorMediator) {
+        this.sponsorshipMediator = sponsorMediator;
+        this.sponsorName = sponsorName;
 
     }
 
-    public String getSponsorName(){
+    public String getSponsorName() {
         return sponsorName;
     }
 
-
-
+    public String toString() {
+        return "\t" + sponsorName;
+    }
 
 }
